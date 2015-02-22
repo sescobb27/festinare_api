17cfb754fb5b7811428542f3835866a546c1162f
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-02-22 12:53:05 -0500

    AWS Configuration

---
+++ AWS.sh
@@ -0,0 +1,35 @@
+sudo apt-get install -y build-essential libffi-dev git-core nodejs npm --no-install-recommends
+git clone https://github.com/sescobb27/hurry-app-discount.git
+sudo npm install -g bower
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo mkdir -p /data/db
+sudo service mongod start
+
+wget http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.0.tar.gz
+tar xvzf ruby-2.2.0.tar.gz
+cd ruby-2.2.0/
+CFLAGS=-fPIC ./configure --enable-shared --disable-install-doc --disable-install-rdoc --disable-install-capi
+make
+sudo make install
+cd ..
+rm -rf ruby-2.2.0 ruby-2.2.0.tar.gz
+echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+echo "export RAILS_ENV='production'" >> .bashrc
+echo "export WEB_CONCURRENCY=\"$(nproc)\"" >> .bashrc
+echo "export PUMA_MAX_THREADS=16" >> .bashrc
+echo "export PUMA_MIN_THREADS=8" >> .bashrc
+echo "export PORT=80" >> .bashrc
+echo "export RACK_ENV='production'" >> .bashrc
+
+sudo gem install bundle
+bundle install
+# RUBY
+cd hurry-app-discount
+gem install bundler
+bower install
+bundle install
+bundle exec puma -C config/puma.rb
