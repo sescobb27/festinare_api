32fe965f07a8671f2292482a0162af245d8cfce8
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-03-07 23:49:44 -0500

    Fixing typo and Load task pipelined

--- app/models/discount_cache.rb
+++ app/models/discount_cache.rb
@@ -10,21 +10,31 @@ class DiscountCache
     def invalidate
       redis = Cache::RedisCache.instance
       now = DateTime.now.to_i
-      len = redis.llen('discount')
+      len = redis.llen('discounts')
       (0..len).each do |x|
-        redis.rpoplpush('discount', 'discount')
-        obj = redis.lrange('discount', 0, 0)[0]
+        redis.rpoplpush('discounts', 'discounts')
+        obj = redis.lrange('discounts', 0, 0)[0]
         break if obj.nil?
-        discount = JSON.parse()
+        discount = JSON.parse(obj)
         expiry_time = DateTime.parse(discount['created_at']).to_i + (discount['duration'] * 60)
         if now > expiry_time
-          redis.lpop('discount')
+          redis.lpop('discounts')
         end
       end
     end
 
     def load
-
+      now = DateTime.now.to_i
+      redis.pipelined do
+        Client.batch_size(500).map do |client|
+          client.discounts.each do |discount|
+            expiry_time = discount.created_at.to_i + discount['duration'] * 60
+            if discount.status && (now < expiry_time)
+              redis.rpush 'discounts', discount
+            end
+          end
+        end
+      end
     end
   end
 end
--- spec/factories/discounts.rb
+++ spec/factories/discounts.rb
@@ -8,7 +8,7 @@ FactoryGirl.define do
     title             { Faker::Product.product }
     secret_key        { "#{title}_secret_key" }
     status            true
-    duration          { [10, 20, 30, 60, 90, 120].sample } # between 0h - 3 hours
+    duration          { Discount::DURATIONS.sample } # between 0h - 3 hours
     created_at        Time.now
     hashtags          { (1..5).map { "##{Faker::Lorem.word}" }}
   end
