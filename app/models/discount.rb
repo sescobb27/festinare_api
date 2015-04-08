5a407a72233a3c0aabc7e51c44251225d05401a7
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-04-07 22:54:31 -0500

    adding more discount's durations

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -19,7 +19,18 @@ class Discount
     index({ hashtags: 1 }, { unique: false })
     default_scope -> { where(status: true) }
     DURATION_TERM = 'minutes'.freeze
-    DURATIONS = [10, 20, 30, 60, 90, 120].freeze
+    DURATIONS = [
+      10,  # 10 minutes
+      20,  # 20 minutes
+      30,  # 30 minutes
+      60,  # 1 hour             => 60 minutes
+      90,  # 1 hour 30 minutes  => 90 minutes
+      120, # 2 hours            => 120 minutes
+      150, # 2 hours 30 minutes => 150 minutes
+      180, # 3 hours            => 180 minutes
+      300, # 5 hours            => 300 minutes
+      360  # 6 hours            => 360 minutes
+    ].freeze
   # =============================END Schema====================================
 
   # =============================Schema Validations============================
