3e2c5c7250755fe219fdede9b057fc01697be0ef
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-04-20 23:06:17 -0500

    Puma loads by default the production environment

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 require 'dotenv'
-ENV.update Dotenv::Environment.new(File.expand_path('.env'))
+ENV.update Dotenv::Environment.new(File.expand_path('.env.production'))
 
 # bundle exec puma -p $PORT -C config/puma.rb
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
@@ -12,11 +12,12 @@ state_path 'log/puma.state'
 
 env_port = ENV['PORT'] && ENV['PORT'] != ''
 port(env_port ? ENV['PORT'] : 3_000)
-environment ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development'
+current_env = ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development'
+environment current_env
 
 on_worker_boot do
-  ENV.update Dotenv::Environment.new(File.expand_path('.env'))
-  Mongoid.load!(File.expand_path('../mongoid.yml', __FILE__), :development)
+  ENV.update Dotenv::Environment.new(File.expand_path('.env.production'))
+  Mongoid.load!(File.expand_path('../mongoid.yml', __FILE__), current_env)
 end
 
 preload_app!
