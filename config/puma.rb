fed580a4645671d1f8076789ff8d44f15a843f81
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-04-25 20:15:18 -0500

    Puma Conf and Assets Cache

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,8 @@
 require 'dotenv'
-ENV.update Dotenv::Environment.new(File.expand_path('.env.production'))
+
+current_env = ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development'
+
+ENV.update Dotenv::Environment.new(File.expand_path(".env.#{current_env}"))
 
 # bundle exec puma -p $PORT -C config/puma.rb
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
@@ -10,13 +13,11 @@ threads min_threads_count, max_threads_count
 pidfile 'tmp/pids/puma.pid'
 state_path 'log/puma.state'
 
-env_port = ENV['PORT'] && ENV['PORT'] != ''
-port(env_port ? ENV['PORT'] : 3_000)
-current_env = ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development'
+port ENV['PORT'] || 3_000
 environment current_env
 
 on_worker_boot do
-  ENV.update Dotenv::Environment.new(File.expand_path('.env.production'))
+  ENV.update Dotenv::Environment.new(File.expand_path(".env.#{current_env}"))
   Mongoid.load!(File.expand_path('../mongoid.yml', __FILE__), current_env)
 end
 
