adf6fc95e29e53b69e4b7347a1eab203e5b88d0f
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-05-02 22:09:51 -0500

    rubocop and how to stop docker containers

--- run.sh
+++ run.sh
@@ -3,3 +3,5 @@ docker run --tty -p 127.0.0.1:27017:27017 -p 127.0.0.1:28017:28017 -d --name="fe
 docker run --tty -p 127.0.0.1:6379:6379 -d --name="festinare_cache" sescobb27/redis:v1
 docker run --tty -p 127.0.0.1::8080 -d --name="festinare_app" --link festinare_db:festinare_db --link festinare_cache:festinare_cache sescobb27/app:v1 ./setup.sh
 docker run --tty -p 80:80 -p 443:443 -d --name="festinare_nginx" --link festinare_app:festinare_app sescobb27/nginx:v1
+
+# docker stop festinare_cache festinare_nginx festinare_app festinare_db
