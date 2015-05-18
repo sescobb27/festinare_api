a3bc5f6568da556f4c52c33209509ae0f346f08b
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-05-18 02:18:55 -0500

    Better conditional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  if Rails.env == 'development'
+  if Rails.env.development?
     Mongoid.logger.level = Logger::DEBUG
     Moped.logger.level = Logger::DEBUG
     Moped.logger = Logger.new($stdout)
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     # render file: "#{Rails.root}/public/index.html", layout: false
     # Rails.application
     # .assets.find_asset('./app/assets/javascripts/app/index.html')
-    if Rails.env == 'development'
+    if Rails.env.development?
       render file: '../assets/javascripts/app/index.html', layout: false
     else
       render file: "#{Rails.root}/public/index.html", layout: false
