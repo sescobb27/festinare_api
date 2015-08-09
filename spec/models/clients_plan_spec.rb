79a49baca02d8058e2d2acb68cbee40e15c2cbae
sescobb27 <s.escob.b.27@gmail.com>
2015-08-08 22:06:37 -0500

    [add] annotate model spec which was renamed

--- spec/models/clients_plan_spec.rb
+++ spec/models/clients_plan_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: clients_plans
+#
+#  id                    :integer          not null, primary key
+#  client_id             :integer
+#  plan_id               :integer
+#  num_of_discounts_left :integer          not null
+#  status                :boolean          default(TRUE)
+#  expired_date          :datetime         not null
+#  created_at            :datetime         not null
+#
+
 require 'rails_helper'
 require 'rake'
 
