fdbe00ba7399d75b14130936d819fc362f8f436b
sescobb27 <s.escob.b.27@gmail.com>
2015-08-16 23:01:53 -0500

    [refactor] remove front-end stuff to a different repo

--- app/assets/javascripts/app/scripts/components/alert/alert-controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('festinare')
-  .controller('AlertCtrl', function ($scope, $mdToast, alert) {
-    $scope.alert = alert;
-    $scope.close = function() {
-      $mdToast.hide();
-    };
-  });
